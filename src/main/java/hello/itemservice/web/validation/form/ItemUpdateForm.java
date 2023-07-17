6670038a77a576fa69bb08dd39700ff5864b6135
gw8413 <gw8413@gmail.com>
2023-07-17 22:58:20 +0900

    Validation V3

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,26 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemUpdateForm {
+
+        @NotNull
+        private Long id;
+
+        @NotBlank
+        private String itemName;
+
+        @NotNull
+        @Range(min = 1000, max = 1000000)
+        private Integer price;
+
+        // 수정에서는 수량이 0개일 수 있으므로 @Max 제약조건을 제거한다.
+        // null도 허용
+        private Integer quantity;
+}
